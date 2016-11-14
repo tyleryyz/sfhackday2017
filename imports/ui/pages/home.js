5c777cd6f3eb9653cccf47994183b9d4796c7531
Tyler Mitchell <tjm375@nau.edu>
2016-11-14 16:46:41 -0700

    logging in now goes to correct page

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -3,6 +3,13 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-Template.home.onRendered(() => {
+import '../components/main_page_buttons.js';
+/*Template.home.onRendered(() => {
   return Meteor.user() ? FlowRouter.go('/main_page') : FlowRouter.go('/');
+})
+*/
+Template.home.helpers({
+  isUser() {
+    return Meteor.user();
+  }
 })
