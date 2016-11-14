4a88bac5de3f2c00cf43f9f5a576a09d16c5973d
Tyler Mitchell <tjm375@nau.edu>
2016-11-14 13:04:01 -0700

    change to log out links

--- imports/ui/components/nav.js
+++ imports/ui/components/nav.js
@@ -5,8 +5,8 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import './nav.html';
 
 Template.nav.events({
-  'click .logout' (event) {
+  'click.logout' (event) {
     Meteor.logout();
-    FlowRouter.go('/home');
+    FlowRouter.go('/');
   }
 })
--- imports/ui/pages/main.js
+++ imports/ui/pages/main.js
@@ -2,3 +2,7 @@ import './main.html';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
+
+Template.home.onCreated(() => {
+  return Meteor.user() ? FlowRouter.go('/') : false
+})
