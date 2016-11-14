6affce180bb70b656f4753e8991d007fcd0b6d1e
Tyler Mitchell <tjm375@nau.edu>
2016-11-14 14:36:50 -0700

    issue fixing

--- imports/ui/pages/home.js
+++ imports/ui/pages/home.js
@@ -3,6 +3,6 @@ import { FlowRouter } from 'meteor/kadira:flow-router';
 import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
 
-Template.home.onCreated(() => {
+Template.home.onRendered(() => {
   return Meteor.user() ? FlowRouter.go('/main_page') : FlowRouter.go('/');
 })
